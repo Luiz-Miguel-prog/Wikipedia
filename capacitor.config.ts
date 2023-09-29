11c36d777d8b1065e4e2b6f0d1ab67b2f185ec08
Luiz Miguel Rodrigues da Rocha <113438364+Luiz-Miguel-prog@users.noreply.github.com>
2023-09-29 11:46:18 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wikipedia',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
